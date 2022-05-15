1354f056475e07fa150829aa42f6d35566f061a3
Hamid Adesokan <aadehamid@gmail.com>
2022-05-15 01:31:28 -0500

    Modified main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
 provider "aws"{
-    region = "us-east-2"
+    region = "us-east-1"
     }
 resource "aws_instance" "hashi_test_instance"{
-    ami = "ami-0fb653ca2d3203ac1"
+    ami = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    user_data = <<-EOF
+                                #!/bin/bash
+                                echo "Hello, World" > index.html
+                                nohup busybox httpd -f -p 8080 &
+                                EOF
     tags = {"name'": "instance_to_test_hashi"}
+}
+resource "aws_security_group" "instance"{
+    name="terraform-example-instance"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
